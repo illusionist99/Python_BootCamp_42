62806a8cb6049ebc86ff02cb91ab78bf77be5f58
Mohamed Alaoui <malaoui@e2r8p4.1337.ma>
2021-04-02 19:31:24 +0100

    main.pynot yet

---
+++ module02/ex01/main.py
@@ -0,0 +1,34 @@
+def what_are_the_vars(*args):
+    index = 0
+    for arg in args:
+        print("var_{index}: {arg}".format(index=index, arg=arg))
+        yield list(arg)
+        index += 1
+    print("end")
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
